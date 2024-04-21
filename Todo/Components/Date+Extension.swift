fc9a5300767e957ed1050579a72f12a7e77e6032
hauntarl <sameer.mungole@gmail.com>
2024-04-21 18:37:49 -0500

    Add date parser and utility methods

--- Todo/Components/Date+Extension.swift
+++ Todo/Components/Date+Extension.swift
@@ -11,6 +11,21 @@ import Foundation
  Extension on Date defines a custom date formatter
  */
 extension Date {
+    /// Outputs date in `"April 21, 2024"` format
+    var formatted: String {
+        Self.dateFormatter.string(from: self)
+    }
+
+    /// Creates a new date from given input using the `dateParser`
+    static func from(_ input: String) -> Self? {
+        Self.dateParser.date(from: input)
+    }
+    
+    /// Converts the date into `ISO6801` format, including micro-seconds
+    var iso8601: String {
+        ISO8601Format(.iso8601WithTimeZone(includingFractionalSeconds: true))
+    }
+    
     private static var dateFormatter: DateFormatter = {
         var formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -18,7 +33,10 @@ extension Date {
         return formatter
     }()
     
-    var formatted: String {
-        Self.dateFormatter.string(from: self)
-    }
+    private static var dateParser: DateFormatter = {
+        var formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss.SSSZ"
+        formatter.locale = .init(identifier: "UTC")
+        return formatter
+    }()
 }
