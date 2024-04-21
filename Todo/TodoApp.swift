37a4c5752cf696cb67bca1c24f81ce4270c8171f
hauntarl <sameer.mungole@gmail.com>
2024-04-21 02:25:15 -0500

    Create stub project

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,18 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Sameer Mungole on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.light)
+        }
+    }
+}
