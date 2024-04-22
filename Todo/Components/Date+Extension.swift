2f6fe40bd200821163046537d6b9b1a9a54261a3
hauntarl <sameer.mungole@gmail.com>
2024-04-21 19:22:09 -0500

    Update date parsers

--- Todo/Components/Date+Extension.swift
+++ Todo/Components/Date+Extension.swift
@@ -11,16 +11,16 @@ import Foundation
  Extension on Date defines a custom date formatter
  */
 extension Date {
+    /// Creates a new date from given input using custom date parsers
+    static func from(_ input: String) -> Self? {
+        Self.dateParserWithFractionalSeconds.date(from: input) ?? Self.dateParser.date(from: input)
+    }
+
     /// Outputs date in `"April 21, 2024"` format
     var formatted: String {
         Self.dateFormatter.string(from: self)
     }
 
-    /// Creates a new date from given input using the `dateParser`
-    static func from(_ input: String) -> Self? {
-        Self.dateParser.date(from: input)
-    }
-    
     /// Converts the date into `ISO6801` format, including micro-seconds
     var iso8601: String {
         ISO8601Format(.iso8601WithTimeZone(includingFractionalSeconds: true))
@@ -33,10 +33,17 @@ extension Date {
         return formatter
     }()
     
-    private static var dateParser: DateFormatter = {
+    static var dateParserWithFractionalSeconds: DateFormatter = {
         var formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss.SSSZ"
-        formatter.locale = .init(identifier: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        formatter.timeZone = .gmt
+        return formatter
+    }()
+    
+    static var dateParser: DateFormatter = {
+        var formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.timeZone = .gmt
         return formatter
     }()
 }
